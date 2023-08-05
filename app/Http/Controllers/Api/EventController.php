687774569c43b0b2f1aec652f70ce15595495b0d
Zbigniew Kwaśniewicz <zbigniew.kwasniewicz@gmail.com>
2023-08-05 22:32:33 +0200

    Adding middleware auth:sanctum for EventController.php without index and show and change hardcoded user_id on store function to actual logged user

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -13,6 +13,11 @@ class EventController extends Controller
 {
     use CanLoadRelationships;
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('index', 'show');
+    }
+
     private array $relations = ['user', 'attendees', 'attendees.user'];
 
     public function index()
@@ -33,7 +38,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_date'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new EventResource($this->loadRelationships($event));
